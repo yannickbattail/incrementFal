dded8c84cb78380a29ab84c64ea21460f277d7e5
Yannick Battail <yannick.battail@raisepartner.com>
2019-06-20 12:28:33 +0200

    change path to Engine

--- zip_release.sh
+++ zip_release.sh
@@ -10,21 +10,21 @@ cp *.js temp_dir/fal/
 cp -R images temp_dir/fal/
 cp -R styles temp_dir/fal/
 
-cp ../json3.min.js temp_dir/
+cp incrementum-ludus/json3.min.js temp_dir/
 
 mkdir temp_dir/Engine
-cp ../Engine/*.js temp_dir/Engine/
+cp incrementum-ludus/Engine/*.js temp_dir/Engine/
 mkdir temp_dir/Engine/implementations
-cp ../Engine/implementations/*.js temp_dir/Engine/implementations/
+cp incrementum-ludus/Engine/implementations/*.js temp_dir/Engine/implementations/
 
 mkdir temp_dir/NodeUpdate
-cp ../NodeUpdate/NodeUpdate.js temp_dir/NodeUpdate/
+cp incrementum-ludus/NodeUpdate/NodeUpdate.js temp_dir/NodeUpdate/
 
 echo "open file fal/index.html" >> temp_dir/readme.txt
 echo "sources can be found here https://github.com/yannickbattail/incrementum-ludus" >> temp_dir/readme.txt
 
 cd temp_dir
-7z a ../incrementFal.zip ./
+7z a incrementFal.zip ./
 cd ..
 
 rm -Rf temp_dir
